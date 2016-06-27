e142b259edbe6e565df0e7e12aef1998e519741e
Rob Coleman <coleman.rob@gmail.com>
2016-06-27 17:11:58 -0400

    chapter 4 complete

--- rango/views.py
+++ rango/views.py
@@ -4,3 +4,9 @@ from django.http import HttpResponse
 def index(request):
     context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake"}
     return render(request, 'rango/index.html', context=context_dict)
+
+def about(request):
+    return render(request, 'rango/about.html')
+
+def colophon(request):
+    return render(request, 'rango/colophon.html')
