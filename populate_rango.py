5f35d0f0a48ee8ef18e6223db7ea2a6e1b9f003d
Rob Coleman <coleman.rob@gmail.com>
2016-06-29 17:37:52 -0400

    finished chapter six

--- populate_rango.py
+++ populate_rango.py
@@ -6,39 +6,47 @@ django.setup()
 
 from rango.models import Category, Page
 
+
 def populate():
+    python_cat = add_cat("Python", views=128, likes=64)
 
-    python_pages = [
-        {"title": "Official Python Tutorial",
-        "url": "http://docs.python.org/2/tutorial/"},
-        {"title":"How to Think like a Computer Scientist",
-        "url":"http://www.greenteapress.com/thinkpython/"},
-        {"title":"Learn Python in 10 Minutes",
-        "url":"http://www.korokithakis.net/tutorials/python/"} ]
+    add_page(cat=python_cat,
+        title="Official Python Tutorial",
+        url="http://docs.python.org/2/tutorial/")
 
-    django_pages = [
-        {"title":"Official Django Tutorial",
-        "url":"https://docs.djangoproject.com/en/1.9/intro/tutorial01/"},
-        {"title":"Django Rocks",
-        "url":"http://www.djangorocks.com/"},
-        {"title":"How to Tango with Django",
-        "url":"http://www.tangowithdjango.com/"} ]
+    add_page(cat=python_cat,
+        title="How to Think like a Computer Scientist",
+        url="http://www.greenteapress.com/thinkpython/")
 
-    other_pages = [
-        {"title":"Bottle",
-        "url":"http://bottlepy.org/docs/dev/"},
-        {"title":"Flask",
-        "url":"http://flask.pocoo.org"} ]
+    add_page(cat=python_cat,
+        title="Learn Python in 10 Minutes",
+        url="http://www.korokithakis.net/tutorials/python/")
 
-    cats = {"Python": {"pages": python_pages},
-        "Django": {"pages": django_pages},
-        "Other Frameworks": {"pages": other_pages} }
+    django_cat = add_cat("Django", views=64, likes=32)
 
-    for cat, cat_data in cats.items():
-        c = add_cat(cat)
-        for p in cat_data["pages"]:
-            add_page(c, p["title"], p["url"])
+    add_page(cat=django_cat,
+        title="Official Django Tutorial",
+        url="https://docs.djangoproject.com/en/1.5/intro/tutorial01/")
 
+    add_page(cat=django_cat,
+        title="Django Rocks",
+        url="http://www.djangorocks.com/")
+
+    add_page(cat=django_cat,
+        title="How to Tango with Django",
+        url="http://www.tangowithdjango.com/")
+
+    frame_cat = add_cat("Other Frameworks", views=32, likes=16)
+
+    add_page(cat=frame_cat,
+        title="Bottle",
+        url="http://bottlepy.org/docs/dev/")
+
+    add_page(cat=frame_cat,
+        title="Flask",
+        url="http://flask.pocoo.org")
+
+    # Print out what we have added to the user.
     for c in Category.objects.all():
         for p in Page.objects.filter(category=c):
             print("- {0} - {1}".format(str(c), str(p)))
@@ -50,11 +58,11 @@ def add_page(cat, title, url, views=0):
     p.save()
     return p
 
-def add_cat(name):
-    c = Category.objects.get_or_create(name=name)[0]
-    c.save()
+def add_cat(name, views, likes):
+    c = Category.objects.get_or_create(name=name, views=views, likes=likes)[0]
     return c
 
+# Start execution here!
 if __name__ == '__main__':
-    print("starting rango population script...")
+    print("Starting Rango population script...")
     populate()
